274ffa4d98e213a77877e43a56ae7e72de042143
Leonid <leonid.manakov@gmail.com>
2023-01-17 15:43:29 +0300

    Solved Task 23

---
+++ Task_23/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+// Метод получения кубов чисел
+
+void GetCubes(int[] number)
+{
+    int length = number.Length;
+
+    for (int i = 1; i < length; i++)
+    {
+        number[i] = i * i * i;
+    }
+}
+
+// Метод печати массива на экран
+
+void PrintArray(int[] coll)
+{
+    int result = coll.Length;
+
+    for (int i = 1; i < result; i++)
+    {
+        Console.Write(coll[i] + " ");
+    }
+}
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+number = Math.Abs(number);
+
+int[] arr = new int[number + 1];
+GetCubes(arr);
+PrintArray(arr);
